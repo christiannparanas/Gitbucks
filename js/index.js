9af5c51df250dd569b83d042af986106db6a55a4
Christian Paranas <christiannparanas@gmail.com>
2020-06-18 02:20:10 +0800

    added result section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const search = document.querySelector('.search')
 const notFound = document.querySelector('.not-found')
 const loader = document.querySelector('.loader')
+const found = document.querySelector('.found')
+
 
 function getUser() {
    const user = document.getElementById('username');
@@ -10,7 +12,7 @@ function getUser() {
 
    } else {
       setTimeout(function() {
-         axios.get(`https://api.github.com/users/${user.value}`).then(res => console.log(res)).catch(function (error) {
+         axios.get(`https://api.github.com/users/${user.value}`).then(res => show(res)).catch(function (error) {
             if (error.response) {
               show(error.response.status);
             }
@@ -27,10 +29,20 @@ function show(res) {
 
       loader.style.display = "none";
       notFound.style.display = "block";
+
+   } else if(res.status == 200){
+
+      loader.style.display = "none";
+      console.log(res.data)
+      document.querySelector('.img').src = res.data.avatar_url;
+      document.querySelector('.name').innerHTML = res.data.name;
+      document.querySelector('.loc').innerHTML = res.data.location;
+      found.style.display = "block";
    }
 }
 
 function back() {
+   found.style.display = "none";
    document.querySelector('#username').value = "";
    notFound.style.display = "none";
    search.style.display = "block"
