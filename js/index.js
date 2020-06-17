219069c06d668e65acffa6aa17b8d8d01609eafb
Christian Paranas <christiannparanas@gmail.com>
2020-06-18 01:13:37 +0800

    not found

--- js/index.js
+++ js/index.js
@@ -1,21 +1,37 @@
-
+const search = document.querySelector('.search')
+const notFound = document.querySelector('.not-found')
+const loader = document.querySelector('.loader')
 
 function getUser() {
-   const user = document.getElementById('username').value;
-   if(user == ""){
-      
-      alert('Please Input username');
+   const user = document.getElementById('username');
+   if(user.value == ""){
+
+      alert("Input Username!")
 
    } else {
-
-      axios({
-         method: 'get',
-         url: `https://api.github.com/users/${user}`
+      setTimeout(function() {
+         axios.get(`https://api.github.com/users/${user.value}`).then(res => console.log(res)).catch(function (error) {
+            if (error.response) {
+              show(error.response.status);
+            }
+          });
    
-      }).then(res => console.log(res)).catch(err => console.error(err));
-   
-      document.querySelector('.search').style.display = "none";
-      document.querySelector('.loader').style.display = "block";
+         search.style.display = "none";
+         loader.style.display = "block";
+      }, 400);
    }
+}
 
+function show(res) {
+   if(res == 404) {
+
+      loader.style.display = "none";
+      notFound.style.display = "block";
+   }
+}
+
+function back() {
+   document.querySelector('#username').value = "";
+   notFound.style.display = "none";
+   search.style.display = "block"
 }
